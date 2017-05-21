8d205b1b306f7c456f69dbfb4073fadd9ed533af
JJ Merelo <jjmerelo@gmail.com>
2017-05-21 18:35:43 +0200

    Adds dummy/doc master module
    Which basically holds a Pod, and throws exception when included, so
    to avoid using it as I did. Yep, dummy me, but well, this might be
    helpful to someone.

---
+++ lib/Math/Sequences.pm
@@ -0,0 +1,23 @@
+=begin pod
+=head1 Math::Sequences
+
+C<Math::Sequences> is a module with integer and floating point sequences and some helper modules
+
+=head1 Synopsis
+
+    use Math::Sequences;
+
+    say factorial(33);
+    
+    say sigma( 8 );
+
+    say FatPi;
+
+    say ℝ.gist();
+
+=end pod
+
+unit module Math::Sequences is export;
+
+fail "This here only for documentation purposes\nTry use Math::Sequences::\{Integer|Real}";
+
